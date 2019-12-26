2fa7d2ed52b7c6c92b5b8fd15ddb27694878004a
Jodi Pranata <jodranata@gmail.com>
2019-12-26 19:43:17 +0700

    Updates

---
+++ precache-manifest.235739e1a3169dd5f285bc80332b373a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03de48a760b6de7d0bb7f0194fb74cf4",
+    "url": "/react-moviedbapp/index.html"
+  },
+  {
+    "revision": "dba4059752f9338a6e98",
+    "url": "/react-moviedbapp/static/css/main.496d5900.chunk.css"
+  },
+  {
+    "revision": "c9255438da7be415b83c",
+    "url": "/react-moviedbapp/static/js/2.9db5a02c.chunk.js"
+  },
+  {
+    "revision": "dba4059752f9338a6e98",
+    "url": "/react-moviedbapp/static/js/main.b1911517.chunk.js"
+  },
+  {
+    "revision": "96281a5c3188b54457ea",
+    "url": "/react-moviedbapp/static/js/runtime-main.23420aa9.js"
+  }
+]);
